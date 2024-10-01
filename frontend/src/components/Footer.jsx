72dbd1aca4d00d8686766b103b27bbffcd411bc4
Yaksh Devani <yaksh9737@gmail.com>
2024-10-01 08:23:53 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
 					</a>
 					. The source code is available on{" "}
 					<a
-						href='https://github.com/burakorkmez'
+						href='https://github.com/yaksh9737'
 						target='_blank'
 						rel='noreferrer'
 						className='font-medium underline underline-offset-4'
