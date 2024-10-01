28a90d00bdde24f75135238aac6238436de3de5e
Yaksh37 <167527566+Yaksh37@users.noreply.github.com>
2024-10-01 07:39:24 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,51 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
+import tvRoutes from "./routes/tv.route.js";
+import searchRoutes from "./routes/search.route.js";
+
+import { ENV_VARS } from "./config/envVars.js";
+import { connectDB } from "./config/db.js";
+import { protectRoute } from "./middleware/protectRoute.js";
+
+const app = express();
+
+const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json()); 
+app.use(cookieParser()); 
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
+
+if (ENV_VARS.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+app.listen(PORT, () => {
+	console.log("Server started at http://localhost:" + PORT);
+	connectDB();
+});
+
+const options = {
+	method: 'GET',
+	headers: {
+	  accept: 'application/json',
+	  Authorization: 'Bearer '+ENV_VARS.TMDB_API_KEY
+	}
+  };
+  
+  fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
